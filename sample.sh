88a765251f47270e65874592815c211b81e2bba4
samshine <sameershinde23243@gmail.com>
2021-02-18 08:56:33 +0530

    use case 5 code added

--- sample.sh
+++ sample.sh
@@ -15,3 +15,13 @@ echo "value for c+a/b:" $result3
 
 result4=$(($a%$b+$c))
 echo "value for a%b+c:" $result4
+
+declare -A result_dict
+result_dict[a+b*c]=$result1
+result_dict[a*b+c]=$result2
+result_dict[c+a/b]=$result3
+result_dict[a%b+c]=$result4
+echo ${result_dict[@]}
+
+
+
